fe880e39b206f45e7e483c40bec69fefa4c30d27
Buffalo Lambdas <lambda75buffalo@gmail.com>
2016-09-24 21:29:56 -0400

    controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,2 @@
+pages_controller.rb
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
-Rails.application.routes.draw do
-  get "/pages/:page" => "pages#show"
+#config/routes.rb
+resources :pages_controller do
+  collection do
+    get :home
+  end
+  	get :about
+  end
+  	get :contact
+  end
 end
