4d081d2c83c0c665430504d34c214faa4220a60f
matthew.zayas <lambda75buffalo@gmail.com>
2016-09-27 02:00:12 -0400

    will it work?

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-
-  get 'pages/about'
-
-  get 'pages/contact'
-
+	def render html: "hello world"
+	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
